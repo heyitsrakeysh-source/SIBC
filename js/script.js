1825cff55fe8348bb8a4e7b461bd55034107abee
itsrakeysh <heyitsrakeysh@gmail.com>
2025-12-08 20:01:18 +0530

    Scroll Button Added

--- js/script.js
+++ js/script.js
@@ -159,3 +159,17 @@ musicToggle.addEventListener('click', () => {
     }
     isPlaying = !isPlaying;
 });
+
+const scrollToTopBtn = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+        scrollToTopBtn.classList.add('show');
+    } else {
+        scrollToTopBtn.classList.remove('show');
+    }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
